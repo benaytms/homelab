830b15d885e9869d21b01a998d1e03d826e599a6
Benay Tomas <115767219+benaytms@users.noreply.github.com>
2026-06-20 23:22:34 -0300

    added comment for subtitle-rename

--- scripts/subtitle-rename.sh
+++ scripts/subtitle-rename.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# because of Jellyfin usage of the ISO 639-1 standard for languages
+# when there's a "pt-BR" in a file name, it thinks it's for Breton (br) instead of portuguese-brazilian
+# so i have to rename all pt-BR subtitles to show portuguese and not breton.
+
 # Rename pt-BR to pt
 # first the main storage
 fdfind '.*\.(pt-BR|pt-br).*\.srt' <my-main-storage> -X rename -v 's/.(pt-BR|pt-br)/\.pt/' {}
